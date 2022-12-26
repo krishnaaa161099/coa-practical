51ad1e590c4959dacd99f5cf4918166c831d5080
krishnaaa161099 <114060634+krishnaaa161099@users.noreply.github.com>
2022-12-26 16:38:16 +0530

    Create Full substractor.cpp

---
+++ Full substractor.cpp
@@ -0,0 +1,29 @@
+//c++ code for above approach
+#include <bits/stdc++.h>
+using namespace std;
+//function to print difference and borrow out
+void Full_Subtractor(int A,int B,int Bin){
+   
+   // Calculating value of Difference
+    int Difference = (A ^ B) ^ Bin;
+       
+    //calculating NOT  value of a
+    int A1 = not(A);
+       
+    //Calculating value of B-Out
+    int B_Out = A1 & Bin | A1 & B | B & Bin;
+   
+    //printing the values
+    cout<<"Difference = "<<Difference<<endl;
+    cout<<"B-Out = "<<B_Out<<endl;
+   
+  }
+//Driver code
+int main() {
+    int A = 1;
+    int B = 0;
+    int Bin = 1;
+    // calling the function
+    Full_Subtractor(A, B, Bin);
+    return 0;
+}
