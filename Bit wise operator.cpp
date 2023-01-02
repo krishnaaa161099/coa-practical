a0410b31a218653fb9e096def4f1e82ff3486d12
krishnaaa161099 <114060634+krishnaaa161099@users.noreply.github.com>
2023-01-02 15:56:08 +0530

    Update Bit wise operator.cpp

--- Bit wise operator.cpp
+++ Bit wise operator.cpp
@@ -1,1 +1,28 @@
+// question 1 - bit wise operators 
+#include <iostream>
+using namespace std;
 
+int main() {
+    int x , y;
+    cin>>x,y;
+
+    cout<<"BITWISE AND" << (x&y) << endl;
+    cout<<"BITWISE OR" << (x|y) <<endl;
+    cout<<"BITWIESE XOR" <<(x^y) <<endl;
+    cout<<"BITWISE NOT" <<(x~y) <<endl;
+    cout<<endl;
+
+    cout<<"BITWISE leftshift ->x"<<(x<<2)<<endl;
+    cout<<"BITWISE leftshift ->y"<<(y<<2)<<endl;
+    cout<<endl;
+
+    cout<<"BITWISE rightshift ->x"<<(x>>2)<<endl;
+    cout<<"BITWISE rightshift ->y"<<(y>>2)<<endl;
+    cout<<endl;
+
+    cout<<"BITWISE NOT -> x"<< (~x)<<endl;
+    cout<<"BITWISE NOT -> Y:"<<(~Y)<<endl;
+    cout<<endl;
+
+    return 0;
+}
